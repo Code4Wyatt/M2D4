7100c8e0e3d1c3b37884a9be06279b11e0dea04b
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-07-08 15:51:01 +0100

    Completed waiting list aspect of JS

--- d4.js
+++ d4.js
@@ -11,9 +11,10 @@
 
 
   function addItem(list, inputField) {    // function with the list and input field as parameters
-    var list = document.getElementById(list)[0];   // variable list declared to target the list, first list since will be adding more later
+    var list = document.getElementById(list);   // variable list declared to target the list, first list since will be adding more later
     var listItem = document.createElement("li");  // variable to create a new list item
     listItem.innerText = inputField.value; // used .innerText to add the value of the input field
     list.appendChild(listItem);  // used append child to add the listItem contents to the list
     return false; // stops submission of form so it doesnt clear and submit one entry each time - i think?
+
   }
