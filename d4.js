5112ca505cbd48806e221bbcbcbeba433e4a684c
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-07-08 21:26:44 +0100

    Trying a new approach

--- d4.js
+++ d4.js
@@ -1,11 +1,13 @@
 
-  function addItem(list, inputField) {    // function with the list and input field as parameters
-    let list = document.getElementById(list);   // variable list declared to target the list, first list since will be adding more later
-    let listItem = document.createElement("li");  // variable to create a new list item
-    listItem.innerText = inputField.value; // used .innerText to add the value of the input field
-    list.appendChild(listItem);  // used append child to add the listItem contents to the list
-    return false; // stops submission of form so it doesnt clear and submit one entry each time - i think?
-  }
+ // function addItem(list, inputField) {    // function with the list and input field as parameters
+ //   var list = document.getElementById(list);   // variable list declared to target the list, first list since will be adding more later
+ //   var listItem = document.createElement("li");  // variable to create a new list item
+// listItem.innerText = inputField.value; // used .innerText to add the value of the input field
+ //   list.appendChild(listItem);  // used append child to add the listItem contents to the list
+ //   return false; // stops submission of form so it doesnt clear and submit one entry each time - i think?
+/  }
 
   
   // How do I get the input box to clear after clicking submit? 
+
+  const inputName = document.querySelector('name')
