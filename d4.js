d59cba787cb852bdea0f31227eeba0de06736f4d
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-07-10 21:19:07 +0100

    Added more notes, understand fully

--- d4.js
+++ d4.js
@@ -1,13 +1,14 @@
 // Create function that adds a name to waiting list
 
 const inputName = document.querySelector("#input-name") // variable to target the name input section (has id #input-name)
-const teamQueue = document.querySelector("#team-queue") //variable to target the waiting list section so we can append to it
+const teamQueue = document.querySelector("#team-queue") // variable to target the waiting list section so we can append to it
 
 function addPerson(event) {
     event.preventDefault()
 
     const li = document.createElement("li") // creating new list element
     li.classList.add("list-group-item") // giving it a class of list-group-item
+    li.innerHTML = inputName.value // making the inner contents of the list item to be the submitted value from name input
 
     teamQueue.appendChild(li) // appending/adding the new li element to the team queue
 }
