08dcd24dbebf6af66a2238552f1d48aad3e9f060
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-07-10 22:03:00 +0100

    Updated

--- d4.js
+++ d4.js
@@ -3,6 +3,9 @@
 const inputName = document.querySelector("#input-name") // variable to target the name input section (has id #input-name)
 const teamQueue = document.querySelector("#team-queue") // variable to target the waiting list section so we can append to it
 
+const teamsN = document.querySelector("#teams-number")
+const teamsRow = document.querySelector("#teams-row")
+
 function addPerson(event) {
     event.preventDefault()
 
@@ -11,4 +14,23 @@ function addPerson(event) {
     li.innerHTML = inputName.value // making the inner contents of the list item to be the submitted value from name input
 
     teamQueue.appendChild(li) // appending/adding the new li element to the team queue
+}
+
+// creating teams section
+
+window.onload = function () {      // window onload to call function when window is displayed
+    createTeams()  // function we are calling
+}
+
+function createTeams() {
+    for (let i = 0; i < teamsN.innerHTML; i++) {    // used for loop to declare index of 0
+                                                        // below used .innerHTML to insert below html element to teamsRow section
+        teamsRow.innerHTML += `<div class="col-3">    
+        <h4>Team ${i + 1}</h4>
+        <ul class="list-group">
+
+        </ul>
+    </div>`
+        
+    }
 }
