e84da5821647223d9d4c0f509785c301809c12c6
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-07-10 21:15:23 +0100

    Updated, need to add innerHTML

--- d4.js
+++ d4.js
@@ -1,4 +1,13 @@
 // Create function that adds a name to waiting list
 
-const inputName = document.querySelector("#input-name"); // variable to target the name input section (has id #input-name)
-const teamQueue = document.querySelector("#team-queue"); variable to target the waiting list section so we can append to it
+const inputName = document.querySelector("#input-name") // variable to target the name input section (has id #input-name)
+const teamQueue = document.querySelector("#team-queue") //variable to target the waiting list section so we can append to it
+
+function addPerson(event) {
+    event.preventDefault()
+
+    const li = document.createElement("li") // creating new list element
+    li.classList.add("list-group-item") // giving it a class of list-group-item
+
+    teamQueue.appendChild(li) // appending/adding the new li element to the team queue
+}
