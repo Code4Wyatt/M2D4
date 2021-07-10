401a57de241edf1a32e8fd360b784c997c043075
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-07-10 21:03:21 +0100

    Updated

--- d4.js
+++ d4.js
@@ -1,1 +1,4 @@
 // Create function that adds a name to waiting list
+
+const inputName = document.querySelector("#input-name"); // variable to target the name input section (has id #input-name)
+const teamQueue = document.querySelector("#team-queue"); variable to target the waiting list section so we can append to it
